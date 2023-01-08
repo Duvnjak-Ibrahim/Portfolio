9ca5438ad5e2f8a14651302f0f5062640bd6580a
Ibrahim Duvnjak <duvnjak.ibrahim2004@gmail.com>
2023-01-08 20:21:21 +0100

    made smooth scroll down function prototype + minor changes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello");
+
+
+// document.addEventListener("scroll",function(){
+//     let y = window.scrollY
+//     console.log(y);
+// })
+
+document.querySelector(".navbar-p")
+.addEventListener("click",function(){
+    let y = window.scrollY
+    // window.scrollBy(y,600)
+    window.scrollTo({
+        top: 735,
+        
+        behavior: 'smooth'
+      });
+      
+      
+})
