4a4eb7d7cd3e4c9f54d7f05a26806e9d850bb4dc
Ibrahim Duvnjak <duvnjak.ibrahim2004@gmail.com>
2023-01-09 22:17:33 +0100

    more significant changes in next section of website

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 
+// ************* this is for that litle house,
+// pixel from up counter
+document.addEventListener("scroll",function(){
+  let y = Math.trunc(window.scrollY)
+    
 
-// document.addEventListener("scroll",function(){
-//     let y = window.scrollY
-//     console.log(y);
-// })
-
+    if(y >= 400){
+      document.querySelector(".scroll-up")
+      .classList.remove("hidden")
+    }else if(y < 200){
+      document.querySelector(".scroll-up")
+      .classList.add("hidden")
+    }
+    
+})
+// ***************** when about is clicked
 document.querySelector(".navbar-p")
 .addEventListener("click",function(){
-    let y = window.scrollY
+  let y = window.scrollY
     // window.scrollBy(y,600)
     window.scrollTo({
-        top: 735,
+        top: 715,
         
         behavior: 'smooth'
       });
       
       
 })
+// ***************** when house is clicked
+  document.querySelector(".scroll-up")
+  .addEventListener("click",function(){
+    let y = window.scrollY
+    
+    window.scrollTo({
+      top:0,
+      behavior:"smooth"
+    })
+  })
